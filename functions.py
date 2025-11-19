1d9b861a94612e2bc8645db28484bd2ac2f4aaef
sareeyahh <sareeyahazamm@gmail.com>
2025-11-18 21:40:00 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,12 +1,10 @@
 def calc_balance(income, expenses):
-    print(f"\nTotal expenses are ${expenses:.2f}")
-    balance = income - expenses 
-    return balance
+    return income - expenses
 
-def financial_status(balance):
+def financial_status_text(balance):
     if balance > 0:
-        print("\nGreat! You are saving money!")
+        return "Great! You are saving money!"
     elif balance == 0:
-     print("\nYou are breaking even.")
+        return "You are breaking even."
     else:
-        print("\n**WARNING** You are overspending!")
+        return "WARNING: You are overspending!"
